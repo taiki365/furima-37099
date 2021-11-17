c2d39231046417477da7cd4f45f3110bcf1ecbdb
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2021-11-17 18:45:39 +0900

    商品情報編集機能　set_item導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :@item = Item.find(params[:id]), only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order(id: 'DESC')
   end
@@ -40,5 +40,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :title, :profile, :category_id, :status_id, :delivery_fee_id, :prefecture_id,
                                  :delivery_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
